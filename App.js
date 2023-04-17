dc985a41ab174ba642f74aa7ffd0f2df01ecd83d
YJH <slade12307@gmail.com>
2023-04-17 20:52:13 +0900

    make nav

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { Feather } from "@expo/vector-icons"
-
+import { Audio } from 'expo-av';
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigator from './app/navigation/AppNavigator'
 const icons={
   "icon1":"thumbs-up"
 }
 export default function App() {
+
   return (
-    <View style={styles.container}>
-     <Feather name="thumbs-up" size={80} color="black" />
-      <Text style={styles.text}> Test </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppNavigator/>
+    </NavigationContainer>
+
   );
 }
 
---
+++ app/navigation/AppNavigator.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AudioList from '../screens/AudioList';
+import Player from '../screens/Player';
+import PlayList from '../screens/PlayList';
+import { Ionicons, MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
+
+
+const Tab = createBottomTabNavigator()
+
+const AppNavigator = () => {
+    return <Tab.Navigator>
+        <Tab.Screen name='AudioList' component={AudioList} options={{
+            tabBarIcon: (color,size)=>{
+return <Ionicons name="headset" size={size} color={color} />
+            }
+        }} />
+        <Tab.Screen name='Player' component={Player}
+        options={{
+            tabBarIcon: (color,size)=>{
+return <FontAwesome5 name="compact-disc" size={size} color={color} />
+            }
+        }}
+        ></Tab.Screen>
+        <Tab.Screen name='PlayList' component={PlayList}
+        options={{
+            tabBarIcon: (color,size)=>{
+return <MaterialIcons name="library-music" size={size} color={color} />
+            }
+        }}
+        ></Tab.Screen>
+    
+    </Tab.Navigator>
+
+}
+
+export default AppNavigator;
